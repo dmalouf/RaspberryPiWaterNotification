e4547536e34157ae00c5ae20fb2e359b9225fced
David Malouf <emailthedavid@gmail.com>
2022-10-08 20:44:14 -0600

    Update main.py

--- main.py
+++ main.py
@@ -14,19 +14,19 @@ def pin_activated(activated_pin):
     if os.environ['on_value'] == incoming_value:
         print(f"pin_activated Log: Activated pin = {activated_pin} - Value = {incoming_value}")
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setwarnings(False)
-GPIO.setup(pin, GPIO.IN)
+if __name__ == "__main__":
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setwarnings(False)
+    GPIO.setup(pin, GPIO.IN)
 
-GPIO.add_event_detect(pin, GPIO.BOTH, bouncetime=300)
-GPIO.add_event_callback(pin, pin_activated)
-GPIO.add_event_callback(pin, AltMessage())
-GPIO.add_event_callback(pin, send_email_message)
-GPIO.add_event_callback(pin, send_sms_message)
+    GPIO.add_event_detect(pin, GPIO.BOTH, bouncetime=300)
+    GPIO.add_event_callback(pin, pin_activated)
+    GPIO.add_event_callback(pin, AltMessage())
+    GPIO.add_event_callback(pin, send_email_message)
+    GPIO.add_event_callback(pin, send_sms_message)
 
+    print(f"Current value of pin: {GPIO.input(pin)}\n")
 
-print(f"Current value of pin: {GPIO.input(pin)}\n")
-
-while True:
-	time.sleep(60)
+    while True:
+        time.sleep(60)
 
